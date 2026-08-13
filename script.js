108451c36c05aa304c2a959bb76578992f9ab9f7
vijeshkumar4545 <vijeshkumar24242424@gmail.com>
2026-08-14 00:48:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,7 @@
     // EXAMPLE — delete or edit these and add your real ones:
 //     {
       id: 1,
-      title: "Supervised Machine Learning: Regression and
-Classification",
+      title: "Supervised Machine Learning: Regression and Classification",
       org: "Coursera/DeepLearning.AI/Stanford",
       date: "28 Dec 2025",
       image: "certificates/supervised_machine_learning.png"
